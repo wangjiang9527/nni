0c9449447f48f3d2a19a6e1c730653a0b2fb04d3
liuzhe <liuzhe.cs@pku.edu.cn>
2020-12-17 13:43:19 +0800

    fix helper script

--- pipelines/tools/_common.py
+++ pipelines/tools/_common.py
@@ -7,6 +7,7 @@ nni_root = str(Path(__file__).parents[2])
 
 def build_wheel():
     python = sys.executable
+    version = sys.argv[1]
     os_spec = {
         'linux': 'manylinux1_x86_64',
         'darwin': 'macosx_10_9_x86_64',
@@ -14,8 +15,8 @@ def build_wheel():
     }[sys.platform]
 
     run_command(f'{python} setup.py clean --all')
-    run_command(f'{python} setup.py build_ts', NNI_RELEASE=sys.argv[1])
-    run_command(f'{python} setup.py bdist_wheel -p {os_spec}', NNI_RELEASE=sys.argv[1])
+    run_command(f'{python} setup.py build_ts', NNI_RELEASE=version)
+    run_command(f'{python} setup.py bdist_wheel -p {os_spec}', NNI_RELEASE=version)
 
     return f'dist/nni-{version}-py3-none-{os_spec}.whl'
 
@@ -24,6 +25,7 @@ def run_command(command, **extra_environ):
         command = command.split()
     subprocess.run(
         command,
+        stderr = subprocess.STDOUT,  # azure will highlight stderr, which is annoying
         cwd = nni_root,
         check = True,
         env = {**os.environ, **extra_environ}
